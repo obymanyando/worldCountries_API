2c1504a87cfd895f0d563f8e267bc05dad059b81
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2020-02-04 01:39:51 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+// declare all global variables
+const flexContainer = document.querySelector('.flex-container')
+const totalCountries = document.querySelector('#counterSpan')
+const searchByName = document.querySelector('.searchByName')
+const searchByCapital = document.querySelector('.searchByCapital')
+const searchByPopulation = document.querySelector('.searchByPop')
+// const inputField = document.getElementsByName('searchField')
+const searchField = document.querySelector('.form__searchField')
+
+
+// Fetch method to be used in production
+
+const url = "https://restcountries.eu/rest/v2/all"
+const fetchCountries = (arr) =>
+{
+    fetch(url)
+        .then(response => response.json())
+        .then(countries =>
+        {
+            let countryList = ''
+            for (const country of countries) {
+
+                let {
+                    name,
+                    capital,
+                    population,
+                    languages,
+                    flag
+                } = country
+
+                // const langs = []
+
+                // for (const language of languages) {
+                //     langs.push(language.name)
+                // }
+
+                const langs = languages.map((langauge) => langauge.name)
+
+
+
+
+                countryList += `<div>
+                            <img src=${flag}>
+                            <h3>${name.toUpperCase()}</h3>
+                            <p>Capital: ${capital}</p>
+                            <p>Languages: ${langs.join(', ')}</p>
+                            <p>Population: ${population.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</p>
+                            </div>`
+
+            }
+            console.log(countries.languages)
+            flexContainer.innerHTML = countryList
+        })
+
+    //total number of countries
+    function numOfCountries()
+    {
+        totalCountries.textContent = countries.length;
+        totalCountries.style.color = 'green'
+        totalCountries.style.fontSize = '20px'
+    }
+    numOfCountries()
+}
+
+
+
+
+
+
+const filterName = () =>
+{
+    flexContainer.innerHTML = ''
+    fetchCountries(countries.reverse())
+}
+
+searchByName.addEventListener('click', filterName)
+
+searchByPopulation.addEventListener('click', filterPop =>
+{
+    const population = countries.filter(country =>
+    {
+        return country.population;
+    });
+    console.log(population);
+    console.log(searchByPopulation)
+})
+
+window.onload = fetchCountries()
