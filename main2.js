0539e0d190838327abe11356c2297fb4cc7fdf1f
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2020-02-04 23:27:00 +0200

    added filtering and some  sorting functionality

--- main2.js
+++ main2.js
@@ -1,6 +1,7 @@
 // declare all global variables
 const flexContainer = document.querySelector('.flex-container')
 const numOfCountries = document.querySelector('#counterSpan')
+const countryCounter = document.querySelector('.filteredCountries')
 const searchByName = document.querySelector('.btn__searchByName')
 const searchByCapital = document.querySelector('.btn__searchByCapital')
 const searchByPop = document.querySelector('.btn__searchByPop')
@@ -8,16 +9,28 @@ const searchField = document.querySelector('.filter__searchField')
 
 // Fetch method to be used in production
 
-// ? fields = name; capital; languages; flag
 
-const url = "https://restcountries.eu/rest/v2/all"
+
+const url = "https://restcountries.eu/rest/v2/all?fields=name;capital;languages;flag"
 
 fetch(url)
     .then(response => response.json())
-    .then(countryData =>
+    .then(countriesData =>
     {
-        const countries = [...countryData]
+        /*..... Note to self: this space is too crowded. Globalise all functions possible.... */
+        const countries = [...countriesData]
         countryBuilder(countries)
+
+        searchField.addEventListener('input', (e) =>
+        {
+            searchTerm = searchField.value
+            countriesList = countries.filter((country) =>
+            {
+                return country.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+            })
+            countryBuilder(countriesList)
+            console.log(countriesList)
+        })
 
         const sortByName = () =>
         {
@@ -26,12 +39,50 @@ fetch(url)
             {
                 return country.name.toLowerCase().startsWith(searchTerm.toLowerCase())
             })
+
+            countryCounter.textContent = `The names of countries starting with - ${searchTerm.toUpperCase()} - are: ${countriesList.length}.`
+            countryCounter.style.marginBottom = '20px'
+            countryCounter.style.color = '#434544'
+            countryCounter.style.backgroundColor = '#f2f7f4'
+            countryCounter.style.padding = '5px'
+
+            countryBuilder(countriesList.reverse())
+
+        }
+
+        const sortByCapital = () =>
+        {
+            searchTerm = searchField.value
+            countriesList = countries.filter((country) =>
+            {
+                return country.capital.toLowerCase().startsWith(searchTerm.toLowerCase())
+            })
+            countryBuilder(countriesList)
+
+
+            countryCounter.textContent = `The number of capital cities starting with - ${searchTerm.toUpperCase()} - is: ${countriesList.length}.`
+            countryCounter.style.marginBottom = '20px'
+            countryCounter.style.color = '#434544'
+            countryCounter.style.backgroundColor = '#f2f7f4'
+            countryCounter.style.padding = '5px'
+        }
+
+        const sortByPop = () =>
+        {
+            countriesList = countries.filter((country) =>
+            {
+                return country.population
+            })
             countryBuilder(countriesList)
         }
 
 
         searchByName.addEventListener('click', sortByName)
+        searchByCapital.addEventListener('click', sortByCapital)
+        searchByPop.addEventListener('click', sortByPop)
     })
+
+/* Function to build HTML */
 
 countryBuilder = (countries) =>
 {
@@ -60,80 +111,21 @@ countryBuilder = (countries) =>
         capital.className = 'country__capital'
         block.className = 'country__block'
         population.className = 'country__pop'
-        // block.style.border = '3px solid #e9e9e9'
-        // block.style.color = '#5e6161'
     }
 
     // display total number of countries
+    const totalCountries = (countries) =>
+    {
+        numOfCountries.textContent = countries.length;
+        numOfCountries.style.color = 'green'
+        numOfCountries.style.backgroundColor = 'whitesmoke'
+        numOfCountries.style.margin = '2px'
+        numOfCountries.style.padding = '1px'
+    }
+    totalCountries(countries)
 
-    numOfCountries.textContent = countries.length;
-    numOfCountries.style.color = 'green'
-    numOfCountries.style.backgroundColor = 'whitesmoke'
-    numOfCountries.style.margin = '2px'
-    numOfCountries.style.padding = '1px'
 
 }
 
 
 
-/* function for event listener on FIRST LETTER search button */
-
-            // const sortByName = () =>
-            // {
-            //     flexContainer.textContent = ''
-            //     countries = searchField.value
-            //     copiedCountries = countries.filter((country) =>
-            //     {
-            //         return country.name.toLowerCase().startsWith(searchTerm.toLowerCase())
-            //     })
-
-            // }
-
-
-            // countryCounter.textContent = `The number of country names starting with - ${searchTerm.toUpperCase()} - is: ${copiedCountries.length}.`
-            // countryCounter.style.marginBottom = '20px'
-            // countryCounter.style.color = '#434544'
-            // countryCounter.style.backgroundColor = '#f2f7f4'
-
-
-
-
-
-            // searchByName.addEventListener('click', sortByName)
-
-/* event listener for dynamic search on input */
-
-
-
-
-// searchByCapital.addEventListener('click', sortByCapital)
-// searchByPop.addEventListener('click', sortByPop)
-
-
-
-/* function for event listener on sort button */
-
-// const sortByName = () =>
-// {
-//     // flexContainer.innerHTML = ''
-
-//     // fetchCountries(country.reverse())
-
-//     console.log(countries.length)
-
-// }
-
-
-
-// searchField.addEventListener('input', (e) =>
-// {
-//     flexContainer.textContent = ''
-//     let searchTerm = e.target.value
-
-//     let copiedCountries = countries.filter((country) =>
-//     {
-//         country.name.toLowerCase().startsWith(searchTerm.toLowerCase())
-//     })
-//     fetchCountries(copiedCountries)
-//     console.log(copiedCountries)
-// })
